cdfc6b991621aaa19dd8ea8a22f4f77522991d1d
BadWolf <romainkowalski@hotmail.fr>
2021-07-18 14:21:38 +0200

    cpp03 done

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -6,15 +6,6 @@
 
 DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name), _name(name)
 {
-	// this->_hitpoints = this->FragTrap::_hitpoints;
-	// this->_energy_points = this->ScavTrap::_energy_points;
-	// this->_attack_damage = this->FragTrap::_attack_damage;
-	// std::cout << "devrait infliger " << this->ScavTrap::_attack_damage << " points de degat" << std::endl;
-	// std::cout << "devrait infliger " << this->FragTrap::_attack_damage << " points de degat" << std::endl;
-	// std::cout << "devrait infliger " << this->ClapTrap::_attack_damage << " points de degat" << std::endl;
-	// std::cout << "possede " << ScavTrap::_energy_points << " energie" << std::endl;
-	// std::cout << "possede " << FragTrap::_energy_points << " energie" << std::endl;
-	// std::cout << "possede " << ClapTrap::_energy_points << " energie" << std::endl;
 	this->_hitpoints = 100;
 	this->_energy_points = 50;
 	this->_attack_damage = 30;
